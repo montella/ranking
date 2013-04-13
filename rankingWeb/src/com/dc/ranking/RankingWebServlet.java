88b550749913770ee683558982431cad1ca7da1d
Carlos A. Córdoba <motella@gmail.com>
2013-04-13 20:53:08 +0200

    Versión inicial

---
+++ rankingWeb/src/com/dc/ranking/RankingWebServlet.java
@@ -0,0 +1,11 @@
+package com.dc.ranking;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class RankingWebServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
