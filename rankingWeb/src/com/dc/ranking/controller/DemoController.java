cfc3f0a7560655f85b2789cd1d28c9d08f4c54a1
Carlos A. Córdoba <motella@gmail.com>
2013-04-18 00:16:41 +0200

    ORM

--- rankingWeb/src/com/dc/ranking/controller/DemoController.java
+++ rankingWeb/src/com/dc/ranking/controller/DemoController.java
@@ -1,15 +1,20 @@
 package com.dc.ranking.controller;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dc.ranking.dao.UserDao;
+import com.dc.ranking.model.UserBean;
 
 @Controller
 public class DemoController 
@@ -17,17 +22,33 @@ public class DemoController
 	/* ================== */
 	/* VARIABLES DE CLASE */
 	/* ================== */
+	
 	private static final Logger log = Logger.getLogger(DemoController.class.getName());
+	
+	@Autowired
+	public UserDao userDao;
 	
 	/* ================ */
 	/* MÉTODOS DE CLASE */
 	/* ================ */
 	
-    @RequestMapping(value="/trenes.htm")
+    @RequestMapping(value="/demo.htm")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) 
     		throws ServletException, IOException 
     {
-    	ModelAndView mav = new ModelAndView("trenes");
+    	ModelAndView mav = new ModelAndView("login");
+    	
+    	int numAleatorio = (int) (Math.random()*4+1);
+    	
+    	UserBean userBean = new UserBean();
+    	userBean.setEmail(numAleatorio + "@gmail.com");
+    	userBean.setFirstName("Carlos");
+    	userBean.setLastName("Córdoba");
+    	userBean.setPassword("Carlos");
+    	
+    	userDao.create(userBean);
+    	
+    	List<UserBean> list = userDao.findAll();
     	
 		mav.addObject("trenes", "Mensaje de prueba");
 		
--- rankingWeb/src/com/dc/ranking/dao/impl/UserDaoImpl.java
+++ rankingWeb/src/com/dc/ranking/dao/impl/UserDaoImpl.java
@@ -2,20 +2,37 @@ package com.dc.ranking.dao.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.dc.ranking.dao.UserDao;
 import com.dc.ranking.model.UserBean;
 
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+
 @Component
 public class UserDaoImpl implements UserDao
 {	
+	
+	/* ********* */
+	/* ATRIBUTOS */
+	/* ********* */
+	
+	@Autowired 
+	private ObjectifyFactory objectifyFactory;
+	
+	/* ******* */
+	/* METHODS */
+	/* ******* */	
+	
 	/**
 	 * Creates a new user.
 	 */
 	public void create(UserBean user)
 	{
-		
+		Objectify ofy = objectifyFactory.begin();
+		ofy.save().entity(user).now();
 	}
 
 	/**
@@ -23,6 +40,9 @@ public class UserDaoImpl implements UserDao
 	 */
 	public List<UserBean> findAll()
 	{
-		return null;
+	    Objectify ofy = objectifyFactory.begin();
+	    ofy.load().type(UserBean.class);
+	    List<UserBean> persons = ofy.load().type(UserBean.class).list();
+	    return persons;
 	}
 }
